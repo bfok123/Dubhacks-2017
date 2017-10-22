c1ad62009f24b6d77a7f5faa871d435715564c31
bch0ng <bchong@uw.edu>
2017-10-22 02:30:14 -0700

    Local Node JS server with login and signup working

--- website/server.js
+++ website/server.js
@@ -17,9 +17,9 @@ var session      = require('express-session');
 var configDB = require('./config/database.js');
 
 // configuration ===============================================================
-mongoose.connect(configDB.url); // connect to our database
+mongoose.connect('mongodb://localhost/users'); // connect to our database
 
-// require('./config/passport')(passport); // pass passport for configuration
+require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
