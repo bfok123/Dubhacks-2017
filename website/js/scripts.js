520038ef38a90be1cb5eb5c8a810ae5ef2e48a97
treytamaki <32915435+treytamaki@users.noreply.github.com>
2017-10-22 00:37:52 -0700

    dont push

--- website/js/scripts.js
+++ website/js/scripts.js
@@ -1,23 +1,35 @@
-<<<<<<< HEAD
 <SCRIPT>
+    function initMap() {
+    var startpos = {lat: 14.05, lng: 0};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: startpos
+    });
+    var marker = new google.maps.Marker({
+        position: startpos,
+        map: map
+    });
+    }
+
+
 function passWord() {
-var testV = 1;
-var pass1 = prompt('Please Enter Your Password',' ');
-while (testV < 3) {
-if (!pass1) 
-history.go(-1);
-if (pass1.toLowerCase() == "letmein") {
-alert('You Got it Right!');
-window.open('www.wikihow.com');
-break;
-} 
-testV+=1;
-var pass1 = 
-prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
-}
-if (pass1.toLowerCase()!="password" & testV ==3) 
-history.go(-1);
-return " ";
+    var testV = 1;
+    var pass1 = prompt('Please Enter Your Password',' ');
+    while (testV < 3) {
+            if (!pass1) 
+            history.go(-1);
+            if (pass1.toLowerCase() == "letmein") {
+            alert('You Got it Right!');
+            window.open('www.wikihow.com');
+            break;
+        } 
+        testV+=1;
+        var pass1 = 
+        prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+    }
+    if (pass1.toLowerCase()!="password" & testV ==3) 
+      history.go(-1);
+    return " ";
 } 
 </SCRIPT>
 <CENTER>
@@ -25,16 +37,3 @@ return " ";
 <input type="button" value="Enter Protected Area" onClick="passWord()">
 </FORM>
 </CENTER>
-=======
-function initMap() {
-  var startpos = {lat: 14.05, lng: 0};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: startpos
-  });
-  var marker = new google.maps.Marker({
-    position: startpos,
-    map: map
-  });
-}
->>>>>>> 02426dc0abbdf25a20c559a5f94a439498405839
