40c7fcc867dbc2311bcb22cf79693dd1c2f449cc
bmoney123 <rangerred101@gmail.com>
2017-10-22 02:32:37 -0700

    added login page and functionality

--- website/js/scripts.js
+++ website/js/scripts.js
@@ -9,3 +9,33 @@ function initMap() {
     map: map
   });
 }
+
+$('#loginButton').on('click', function() {
+  if($('#username').val().length == 0 || $('#password').val().length == 0) {
+    alert('please enter a username and password');
+  } else {
+    firebase.database().ref('/users/').child($('#username').val()).once('value', function(snapshot) {
+      if(snapshot.child('username').val() === $('#username').val() && snapshot.child('password').val() === $('#password').val()) {
+        alert('logged in');
+      } else {
+        alert('that user does not exist');
+      }
+    });
+  }
+/*
+  firebase.database().ref('/users/' + $('#username').val()).once('value', function(snapshot) {
+    snapshot.forEach(function(snapshotChild) {
+      //if(snapshotChild.val() === )
+    });
+  });*/
+});
+
+$("#registerButton").on("click", function() {
+  if($('#username').val().length == 0 || $('#password').val().length == 0) {
+    alert('please enter a username and password');
+  }
+  firebase.database().ref('/users/' + $('#username').val()).set({
+    username: $('#username').val(),
+    password: $('#password').val()
+  });
+});
