7280cc6098213042db303453c97aceca29715ff8
bmoney123 <rangerred101@gmail.com>
2017-10-22 00:25:36 -0700

    style changes, map width, divider, map title, etc.

---
+++ website/js/scripts.js
@@ -0,0 +1,11 @@
+function initMap() {
+  var startpos = {lat: 14.05, lng: 0};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: startpos
+  });
+  var marker = new google.maps.Marker({
+    position: startpos,
+    map: map
+  });
+}
